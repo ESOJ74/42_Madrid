9353c9db92125567758eb871ca80200a2e7a2331
Jose luis Cuenca lagunas <jcuenca-@c2r4s5.42madrid.com>
2021-08-16 19:43:27 +0200

    .

--- 100/C Piscine C 05/test/main04.c
+++ 100/C Piscine C 05/test/main04.c
@@ -4,7 +4,7 @@ int	ft_fibonacci(int index);
 
 int main (void)
 {
-	printf("\n\nex04 : ft_fibonacci(4);\nLa salida debe ser 144\nSalida: ");
+	printf("\n\nex04 : ft_fibonacci(12);\nLa salida debe ser 144\nSalida: ");
 	printf("%d\n", ft_fibonacci(12));
 	return(0);
 }
